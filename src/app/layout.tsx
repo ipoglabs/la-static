ce7ebb21cb2bf727f70afa904554c270f8b8e34b
unknown <jp.naga85@gmail.com>
2026-04-09 18:19:06 +0530

    layout changed with meta data viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import './globals.css'
 
 import Header from '@/components/Header'
@@ -7,6 +7,14 @@ import Footer from '@/components/Footer'
 export const metadata: Metadata = {
   title: 'LokalAds',
   description: 'Find anything with LokalAds',
+}
+
+// ✅ MOVE viewport here (separate export)
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover',
+  userScalable: false,
 }
 
 export default function RootLayout({
