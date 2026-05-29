53538e7e4929f783684f3c53a4ce7f12ffa124e1
Jayapriya0803 <jp.naga85@gmail.com>
2026-05-29 14:33:39 +0530

    donate error

--- src/app/api/create-payment-intent/route.ts
+++ src/app/api/create-payment-intent/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2024-06-20",
+  apiVersion: "2026-05-27.dahlia",
 });
 
 // Supported currencies per country
